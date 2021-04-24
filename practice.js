8645412a647d06babaf958af43a70e84f766e891
TangoBr <thomasbridges01@gmail.com>
2021-04-24 12:02:26 -0400

    practice

---
+++ practice.js
@@ -0,0 +1,11 @@
+let str = (prompt("Enter your name"))
+
+var count = function() {
+    return str.length
+}
+
+
+console.log(count())
+
+// let str1 = "Morgan"
+// console.log(`${str1} ${str1.length}`)
